b3544bc6bc1de820822a845796ea4fdca006b906
Elias May <elias.may13@gmail.com>
2019-01-05 01:46:24 -0600

    Add molecule distributions

--- logic.js
+++ logic.js
@@ -5,8 +5,9 @@ var num_rows = null;
     canv = null;
     ctx = null;
     game_board = null;
+    mtype = null;
 
-let Molecule = class {
+/*let Molecule = class {
 
   constructor(type) {
     if (type=="o"){
@@ -21,6 +22,22 @@ let Molecule = class {
   flip(){
     //todo scramble positions
   }
+}*/
+
+function Molecule(type){
+  if (type=="o"){
+    this.type = type;
+    this.n = 1;
+    this.e = 1;
+    this.s = 1;
+    this.w = 1;
+  }
+  this.flip();
+}
+Molecule.prototype.flip = function() {
+};
+Molecule.prototype.toString = function() {
+  return this.type;
 }
 
 function start(){
@@ -29,11 +46,13 @@ function start(){
   num_rows = $("#rows").val();
   num_cols = $("#cols").val();
   concentration = $("#dist").val();
+  mtype = $("#mtype").val();
 
   block_size = Math.min(canv.height / num_rows, canv.width / num_cols)
   game_board = createBoard();
-  m = new Molecule('o');
-  game_board[2][0] = m;
+  createMolecules();
+
+
 
 
 
@@ -83,14 +102,18 @@ function drawMolecule(x, y){
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
 
 function debug(){
   console.log("num_rows: " + num_rows);
   console.log("num_cols: " + num_cols);
   console.log("block_size: " + block_size);
   console.log("concentration: " + concentration);
+  console.log("molecule type: " + mtype);
   console.log("gameboard: ");
-  for (x = 0; x < game_board.length; x++) {
+  for (var x = 0; x < game_board.length; x++) {
     console.log(game_board[x].toString());
   }
 
@@ -107,4 +130,24 @@ function createBoard(){
     game_board.push(array);
   }
   return game_board;
+}
+function createMolecules(){
+  var total_spots = num_rows * num_cols;
+  var spots_to_fill = Math.round(total_spots * concentration)
+
+  for (var x = 0; x < spots_to_fill; x++) {
+    injectMolecule(mtype);
+  }
+}
+function injectMolecule(type){
+  var m = new Molecule(mtype);
+  var flag = true;
+  while (flag){
+    var x = random(0, game_board.length);
+    var y = random(0, game_board[0].length);
+    if (game_board[x][y] == null){
+      game_board[x][y] = m;
+      flag = false;
+    }
+  }
 }
